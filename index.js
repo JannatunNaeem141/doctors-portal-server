cec97f8d9786292583a96de7d2f950097fdec301
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-16 10:55:08 +0600

    Created api for dashboard

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ async function run() {
          * app.delete('/booking/:id')
          */
 
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
